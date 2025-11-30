88016d4e7358c92fc0eec01c27763b013a5f4704
ajinkya-cell <ajinkyaadharkar@gmail.com>
2025-11-30 22:51:01 +0530

    first commit

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,58 @@
+import { UIMessage } from "@ai-sdk/react"
+import {streamText , convertToModelMessages, tool , InferUITools , UIDataTypes , stepCountIs} from "ai"
+import {createOpenRouter} from '@openrouter/ai-sdk-provider'
+import { z } from "zod"
+import { searchDocuments } from "@/lib/search"
+
+const tools = {
+    searchKnowledgeBase : tool({
+        description:"Search the Knowledge base for relevant information",
+        inputSchema: z.object({
+            query : z.string().describe("The search query to find relevant document")
+        }),
+        execute : async ({ query }) =>{
+          try{
+const results = await searchDocuments(query , 3 , 0.5)
+if(results.length === 0){
+    return " No relevant information found in the knowledge base"
+}
+        const formattedResults = results
+        .map((r , i)=>`[${i+1}] ${r.content}`)
+        .join("\n\n")
+
+        return formattedResults;
+          }  catch(error){
+            console.error("Search error" , error)
+            return "Error searching the knowledge base"
+          }
+        }
+    })
+}
+
+export type ChatTools = InferUITools<typeof tools>
+export type ChatMessage = UIMessage<never , UIDataTypes , ChatTools>
+
+const openrouter = createOpenRouter({
+    apiKey: process.env.OPEN_ROUTER_API_KEY
+})
+
+export async function POST(req:Request){
+    try {
+        const {messages}:{messages:ChatMessage[]} = await req.json()
+    const result =await streamText({
+         model: openrouter.chat('x-ai/grok-4.1-fast:free'),
+         messages :convertToModelMessages(messages),
+         tools,
+          system: `You are a helpful assistant with access to a knowledge base. 
+          When users ask questions, search the knowledge base for relevant information.
+          Always search before answering if the question might relate to uploaded documents.
+          Base your answers on the search results when available. Give concise answers that correctly answer what the user is asking for. Do not flood them with all the information from the search results.`,
+      stopWhen: stepCountIs(2),
+    })
+
+    return result.toUIMessageStreamResponse()
+    } catch (error) {
+        console.error("Error streamig chat completion" , error)
+        return new Response("Failed to stream chat completion" ,{status:500})
+    }
+}
---
+++ src/lib/embeddings.ts
@@ -0,0 +1,27 @@
+import { embed , embedMany } from "ai"
+import { createMistral } from "@ai-sdk/mistral";
+import { NextResponse } from 'next/server';
+
+const mistral = createMistral({
+  apiKey: process.env.MISTRAL_API_KEY
+});
+
+export async function generateEmbedding(text : string){
+    const input = text.replace("\n" , " ");
+
+    const { embedding } = await embed({
+        model:mistral.embedding("mistral-embed"),
+        value:input
+    })
+    return embedding;
+    
+}
+
+export async function generateEmbeddings(texts : string[]){
+    const inputs = texts.map((text)=> text.replace("\n" , " "))
+    const { embeddings } = await embedMany({
+        model:mistral.textEmbeddingModel('mistral-embed'),
+        values:inputs
+    })
+    return embeddings;
+}
